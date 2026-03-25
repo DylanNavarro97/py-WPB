3f81424211ecf08ff77b15f97f76139141709265
Dylan Navarro <dylanrubennavarro@gmail.com>
2026-03-25 14:01:47 -0300

    webhook configurado

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, request
 from dotenv import load_dotenv
 import os
+import hmac
+import hashlib
 
 app = Flask(__name__)
+
+env_verify_token = os.getenv("WP_HUB_TOKEN")
+WP_KEY = os.getenv("WP_KEY")
 
 @app.route("/", methods=["GET"])
 def home():
@@ -12,14 +17,34 @@ def home():
 def verify():
     token = request.args.get("hub.verify_token")
     challenge = request.args.get("hub.challenge")
-    env_verify_token = os.getenv("WP_KEY")
+    
 
-    if token ==  env_verify_token:
+    if token == env_verify_token:
         return challenge, 200
     else:
         return "error", 403
 
+
+@app.route("/webhook", methods=["POST"])
+def load_webhook():
+    signature = request.headers.get("X-Hub-Signature-256")
+
+    body = request.data
+
+    # expected_signature = "sha256=" + hmac.new(
+    #     WP_KEY.encode(),
+    #     body,
+    #     hashlib.sha256
+    # ).hexdigest()
+
+    # if signature != expected_signature:
+    #     return "firma inválida", 403
     
+    data = request.json()
+    print(data)
+
+    
+        
 
 if __name__ == "__main__":
     app.run(debug=True)
